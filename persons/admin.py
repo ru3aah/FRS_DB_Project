a9d3276a511632793f5f9d9248378894422171d1
papanda <ru3aah@gmail.com>
2026-02-03 03:00:59 +0400

    persons app adjasted

--- persons/admin.py
+++ persons/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+# persons/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Person
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = (
+        "person_id",
+        "first_name",
+        "second_name",
+        "family_name",
+        "dob",
+        "gender",
+        "created_at",
+        "updated_at",
+    )
+    list_display_links = ("person_id",)
+    search_fields = ("dob", "first_name", "second_name", "family_name", "gender")
+    ordering = ("family_name", "first_name", "dob")
