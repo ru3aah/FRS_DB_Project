caf6fc79daf492e2634eb9b6f6f9c36d0a2570d7
papanda <ru3aah@gmail.com>
2026-02-03 01:56:09 +0400

    class Person created

--- persons/models.py
+++ persons/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    person_id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+    family_name = models.CharField(max_length=255)
+    dob = models.DateField()
+    gender = models.CharField(max_length=1,
+                              choices=(('M', 'Male'), ('F', 'Female')),
+                              default='M',
+                              blank=False,
+                              null=False,
+                              )
