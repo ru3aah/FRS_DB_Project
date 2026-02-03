8cd9eb75472f32fd7b9d132751f0621b78736991
papanda <ru3aah@gmail.com>
2026-02-03 16:18:14 +0100

    templates updated

--- persons/urls.py
+++ persons/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = "persons"
+
 urlpatterns = [
-    path("", views.index, name="index"),  # список всех Person
+    path("index/", views.index, name="index"),  # список всех Person
     path("person/<int:pk>/", views.person_detail, name="detail"),
 ]
