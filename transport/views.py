f94e55790248bc79391282367ff9a40f8ac154da
papanda <ru3aah@gmail.com>
2026-04-03 11:53:29 +0300

    views updated followin models update

--- transport/views.py
+++ transport/views.py
@@ -1,14 +1,10 @@
 from django import forms
 from django.http import Http404
 from django.urls import reverse_lazy
-from django.views.generic import (
-    CreateView,  # noqa: I001
-    ListView,
-    TemplateView,
-)
+from django.views.generic import CreateView, ListView, TemplateView
 
 from companies.models import Company
-from transport.models import TransportUnit, TransportUnitType
+from transport.models import TransportUnit, TransportUnitModel, TransportUnitType
 
 
 class TransportHomeView(TemplateView):
@@ -59,7 +55,10 @@ class TransportUnitCreateView(CreateView):
         form = super().get_form(form_class)
         field = form.fields["type"]
         assert isinstance(field, forms.ModelChoiceField)
-        field.queryset = TransportUnitType.objects.filter(is_active=True)
+        field.queryset = TransportUnitType.objects.filter(
+            company_id=self.active_company_id,
+            is_active=True,
+        )
         return form
 
     def form_valid(self, form):
@@ -77,12 +76,103 @@ class TransportUnitTypeListView(ListView):
     model = TransportUnitType
     template_name = "transport/type_list.html"
     context_object_name = "types"
-    queryset = TransportUnitType.objects.all().order_by("name")
+
+    def get_queryset(self):
+        active_company_id = self.request.session.get("active_company_id")
+
+        queryset = TransportUnitType.objects.all().order_by("name")
+
+        if active_company_id:
+            queryset = queryset.filter(company_id=active_company_id)
+        else:
+            queryset = queryset.none()
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["active_company_id"] = self.request.session.get("active_company_id")
+        return context
 
 
 class TransportUnitTypeCreateView(CreateView):
+    active_company_id: int | None = None
     model = TransportUnitType
     template_name = "transport/type_form.html"
     fields = ["code", "name", "description", "is_active"]
-    extra_context = {"active_company_id": None}
     success_url = reverse_lazy("transport:type_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        self.active_company_id = request.session.get("active_company_id")
+        if not self.active_company_id:
+            raise Http404("Active company is not selected.")
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.company_id = self.active_company_id
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["active_company_id"] = self.active_company_id
+        return context
+
+
+class TransportUnitModelListView(ListView):
+    model = TransportUnitModel
+    template_name = "transport/model_list.html"
+    context_object_name = "models"
+
+    def get_queryset(self):
+        active_company_id = self.request.session.get("active_company_id")
+
+        queryset = (
+            TransportUnitModel.objects.select_related("company", "type")
+            .all()
+            .order_by("type__name", "name")
+        )
+
+        if active_company_id:
+            queryset = queryset.filter(company_id=active_company_id)
+        else:
+            queryset = queryset.none()
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["active_company_id"] = self.request.session.get("active_company_id")
+        return context
+
+
+class TransportUnitModelCreateView(CreateView):
+    active_company_id: int | None = None
+    model = TransportUnitModel
+    template_name = "transport/model_form.html"
+    fields = ["type", "name", "code", "description", "is_active"]
+    success_url = reverse_lazy("transport:model_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        self.active_company_id = request.session.get("active_company_id")
+        if not self.active_company_id:
+            raise Http404("Active company is not selected.")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        field = form.fields["type"]
+        assert isinstance(field, forms.ModelChoiceField)
+        field.queryset = TransportUnitType.objects.filter(
+            company_id=self.active_company_id,
+            is_active=True,
+        ).order_by("name")
+        return form
+
+    def form_valid(self, form):
+        form.instance.company_id = self.active_company_id
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["active_company_id"] = self.active_company_id
+        return context
