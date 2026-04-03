7f17ba235c8a5a73cde71570dd2d53c28b618720
papanda <ru3aah@gmail.com>
2026-04-03 08:03:51 +0300

    add urls for unitlistview & unittypelistview

--- transport/urls.py
+++ transport/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import TransportHomeView
+from .views import TransportHomeView, TransportUnitListView, TransportUnitTypeListView
 
 app_name = "transport"
 
 urlpatterns = [
     path("", TransportHomeView.as_view(), name="index"),
+    path("units/", TransportUnitListView.as_view(), name="unit_list"),
+    path("types/", TransportUnitTypeListView.as_view(), name="type_list"),
 ]
